951ca8a98cd8f3ecb27392df2162a7b9345f6c42
Evan Neidler <project.relyxx@gmail.com>
2024-05-21 15:33:43 -0400

    Adds functionality to compare calculated diameter with the saved diameter of selected line. Changes background red or green depending on if diameter is in spec.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
+import redX from './images/red_x.svg'
+import greenCheck from './images/green_check.svg'
 import { production_lines } from './js/plant-data.js';
 import { calculateRollDiameter } from './js/diameter-calculator.js';
 
@@ -12,7 +14,9 @@ document.querySelector('#line-select').innerHTML = `
 // Variable for the line selection dropdown
 const lineSelectionDropdown = document.querySelector('#line-select');
 
-lineSelectionDropdown.addEventListener("change", changeHandler)
+let diaBackground;
+
+lineSelectionDropdown.addEventListener("change", changeHandler);
 
 // Initial value on page load for dropdown label
 document.getElementById('line-select-label').innerText = "Select a Manufacturing Line";
@@ -20,7 +24,11 @@ document.getElementById('line-select-label').innerText = "Select a Manufacturing
 // Handles the DOM changes for switching the line select dropdown
 function changeHandler(){
   const result = production_lines.filter((line) => lineSelectionDropdown.options[lineSelectionDropdown.selectedIndex].text === line.name);
-  document.getElementById('content').innerHTML = ` ${result.map((item) => (`<h3>${item.name}</h3><p>${item.description}</p>`))}` // <img src=${item.image} alt=${item.name}/>
+
+  document.getElementById('content').innerHTML = ` ${result.map((item) => (`
+  <h3>${item.name}</h3>
+  <p>${item.description}</p>
+  `))}` 
 }
 
 // This is the id of the button
@@ -28,8 +36,12 @@ document.getElementById('calculate-diameter').onclick = calculateClickHandler;
 
 export function calculateClickHandler(){
     const outer_diameter = calculateRollDiameter(); // The calculate funtion works as intended
+    const result = production_lines.filter((line) => lineSelectionDropdown.options[lineSelectionDropdown.selectedIndex].text === line.name);
 
-    document.getElementById('calculated-dia').innerText = `${outer_diameter.toFixed(2)}"`
+    diaBackground = (outer_diameter > result.map(item => item.max_dia)) ? 'dia-not-okay' : 'dia-okay'
+    
+    document.getElementById('calculated-dia').innerText = `${outer_diameter.toFixed(2)}`
+    document.getElementById('content').setAttribute('class', diaBackground)
 }
 
 
